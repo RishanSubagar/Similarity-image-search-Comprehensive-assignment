492a3f0fa3076c8bf00ae93839b0438648e241cd
RishanSubagar <rishans48@gmail.com>
2024-02-17 21:21:54 -0500

    Start ColorImage methods

--- ColorImage.java
+++ ColorImage.java
@@ -1,3 +1,75 @@
-public class ColorImage {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class ColorImage {
+    private int width;
+    private int height;
+    private int depth; // Number of bits per pixel
+    private int[][][] pixels; // Array representation of pixel values
+
+    // Constructor that creates an image from a file (supports jpg and ppm formats)
+    public ColorImage(String filename) {
+        if (filename.toLowerCase().endsWith(".ppm")) {
+            readPpmImage(filename);
+        } else {
+            System.err.println("Unsupported image format.");
+        }
+    }
+
+    private void readPpmImage(String filename) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            // Read PPM header
+            br.readLine();
+            String[] dimensions = br.readLine().split(" ");
+            width = Integer.parseInt(dimensions[0]);
+            height = Integer.parseInt(dimensions[1]);
+            depth = Integer.parseInt(br.readLine());
+            pixels = new int[width][height][3];
+
+            // Read pixel values
+            for (int j = 0; j < height; j++) {
+                for (int i = 0; i < width; i++) {
+                    pixels[i][j][0] = Integer.parseInt(br.readLine());
+                    pixels[i][j][1] = Integer.parseInt(br.readLine());
+                    pixels[i][j][2] = Integer.parseInt(br.readLine());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Getter methods for image attributes
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getDepth() {
+        return depth;
+    }
+
+    // Get the 3-channel value of pixel at column i, row j
+    public int[] getPixel(int i, int j) {
+        return pixels[i][j];
+    }
+
+    // Reduce the color space to a d-bit representation
+    public void reduceColor(int d) {
+        // Implement your logic to reduce the color space to d bits
+        // You may need to scale the pixel values accordingly
+        // This is specific to the PPM format, adapt as needed for your use case
+        int scale = (int) Math.pow(2, depth - d);
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                for (int c = 0; c < 3; c++) {
+                    pixels[i][j][c] = (pixels[i][j][c] / scale) * scale;
+                }
+            }
+        }
+    }
 }
